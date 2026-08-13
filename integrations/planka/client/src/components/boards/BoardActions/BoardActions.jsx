4c5917619e8c4f8048cf7efda3b4d2c30709d909
João Carlos Pereira <jcp2105@gmail.com>
2026-08-12 23:22:09 -0300

    fix(planka): hide board member avatars

--- integrations/planka/client/src/components/boards/BoardActions/BoardActions.jsx
+++ integrations/planka/client/src/components/boards/BoardActions/BoardActions.jsx
@@ -14,7 +14,6 @@ import { BoardContexts } from '../../../constants/Enums';
 import { BoardContextIcons } from '../../../constants/Icons';
 import Filters from './Filters';
 import RightSide from './RightSide';
-import BoardMemberships from '../../board-memberships/BoardMemberships';
 
 import styles from './BoardActions.module.scss';
 
@@ -22,20 +21,6 @@ const BoardActions = React.memo(() => {
   const boardContext = useSelector((state) => selectors.selectCurrentBoard(state).context);
 
   const withContextTitle = boardContext !== BoardContexts.BOARD;
-
-  const withMemberships = useSelector((state) => {
-    if (withContextTitle) {
-      return false;
-    }
-
-    const boardMemberships = selectors.selectMembershipsForCurrentBoard(state);
-
-    if (boardMemberships.length > 0) {
-      return true;
-    }
-
-    return selectors.selectIsCurrentUserManagerForCurrentProject(state);
-  });
 
   const [t] = useTranslation();
 
@@ -48,11 +33,6 @@ const BoardActions = React.memo(() => {
               <Icon name={BoardContextIcons[boardContext]} className={styles.contextTitleIcon} />
               {t(`common.${boardContext}`)}
             </div>
-          </div>
-        )}
-        {withMemberships && (
-          <div className={styles.action}>
-            <BoardMemberships />
           </div>
         )}
         <div className={styles.action}>
